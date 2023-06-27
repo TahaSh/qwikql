7dbc7fda136e4bdcdcbb8c91ca79c679adf36154
Leifer Mendez <leifer33@gmail.com>
2023-06-27 14:34:11 +0200

    fix: :zap: change context to contextId

--- src/contexts.ts
+++ src/contexts.ts
@@ -1,10 +1,10 @@
-import { createContext, QRL } from '@builder.io/qwik'
+import { createContextId, QRL } from '@builder.io/qwik'
 
-export const QwikqlURLContext = createContext<{ url: string }>('qwikql.url')
-export const QwikqlRequestContextContext = createContext<{
+export const QwikqlURLContext = createContextId<{ url: string }>('qwikql.url')
+export const QwikqlRequestContextContext = createContextId<{
   headers: Record<string, string>
 }>('qwikql.requestContext')
 export const QwikqlSetHeadersContext =
-  createContext<QRL<(headers: Record<string, string>) => void>>(
+createContextId<QRL<(headers: Record<string, string>) => void>>(
     'qwikql.setHeaders'
   )
