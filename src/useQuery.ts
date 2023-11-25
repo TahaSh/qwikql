ae1d171ce38106e347fa47946df94eb1fb4e8773
Jean-Charles Fct <fauchet.jeancharles@gmail.com>
2023-11-25 09:30:33 +0100

    feat(request): add timeout (#7)
    Thanks to @jcfauchet for creating this PR!

--- src/useQuery.ts
+++ src/useQuery.ts
@@ -1,6 +1,10 @@
 import { useContext, $ } from '@builder.io/qwik'
 import { request, RequestDocument } from 'graphql-request'
-import { QwikqlRequestContextContext, QwikqlURLContext } from './contexts'
+import {
+  QwikqlRequestContextContext,
+  QwikqlTimeoutContext,
+  QwikqlURLContext
+} from './contexts'
 import { toQwikqlError } from './util/toQwikqlError'
 
 interface QueryConfig {
@@ -11,17 +15,29 @@ export const useQuery = (query: RequestDocument) => {
   const queryAsString = query.toString()
   const url = useContext(QwikqlURLContext).url
   const requestContext = useContext(QwikqlRequestContextContext)
+  const timeout = useContext(QwikqlTimeoutContext).timeout
 
   const executeQuery$ = $(async (queryConfig: Partial<QueryConfig> = {}) => {
+    let controller: AbortController | undefined, timeoutId
+    if (timeout) {
+      controller = new AbortController()
+      timeoutId = setTimeout(() => {
+        controller!.abort()
+      }, timeout)
+    }
+
     try {
       return await request({
         url,
         document: queryAsString,
         variables: queryConfig.variables || undefined,
-        requestHeaders: requestContext.headers
+        requestHeaders: requestContext.headers,
+        signal: controller?.signal
       })
     } catch (error) {
       return Promise.reject(toQwikqlError(error))
+    } finally {
+      if (timeoutId) clearTimeout(timeoutId)
     }
   })
 
